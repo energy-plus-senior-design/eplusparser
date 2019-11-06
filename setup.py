1db2dbe4986b4d33670deda83ae2df1dddc13ac9
Sameer Bibikar <bibikar@utexas.edu>
2019-11-06 13:28:52 -0600

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='eplusparser',
-        version='0.1.0',
+        version='0.1.1',
         description='Parser for EnergyPlus SQL output',
         packages=find_packages(),
         python_requires='>=3.5',
